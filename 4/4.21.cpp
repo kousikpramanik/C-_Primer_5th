59db2ab3768874f00669449918996359730db4d2
Chandradeep Dey <chandradeepdey@posteo.net>
2019-09-19 00:29:56 +0530

    Add till 4.21

---
+++ 4/4.21.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+        std::vector<int> vect;
+
+        std::cout << "Enter integers (EOF/invalid input to stop)." <<
+                  std::endl;
+        for (int temp; std::cin >> temp;)
+                vect.push_back(temp);
+
+        std::cout << "Here is the vector: ";
+        for (const auto &n: vect)
+                std::cout << n << ' ';
+        std::cout << std::endl;
+
+        std::cout << "Now doubling odd values." << std::endl;
+        for (auto &n : vect)
+                ((n % 2) == 0) ?: n *= 2;
+
+        std::cout << "Here is the vector again: ";
+        for (const auto &n : vect)
+                std::cout << n << ' ';
+        std::cout << std::endl;
+
+        return 0;
+}
