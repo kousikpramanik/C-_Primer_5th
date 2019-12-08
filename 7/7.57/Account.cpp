b02fb7b3f611f4853c436cd7f4a49b677b3ec72d
Chandradeep Dey <chandradeepdey@posteo.net>
2019-12-08 21:07:20 +0530

    Chapter 7 Done

---
+++ 7/7.57/Account.cpp
@@ -0,0 +1,5 @@
+#include "Account.h"
+
+double Account::interestRate = 0.0;
+
+bool Account::interestDay = false;
---
+++ 7/7.57/Account.h
@@ -0,0 +1,57 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <string>
+
+class Account {
+    friend std::istream &read(std::istream &is, Account &obj);
+
+    friend std::ostream &print(std::ostream &os, const Account &obj);
+
+public: // constructors
+    //Account and std::string are quite different things, so explicit
+    explicit Account(const std::string &o) : owner(o) {}
+
+    Account(const std::string &o, double a) : owner(o), amount(a) {}
+
+    // Account and std::istream& are quite different things
+    explicit Account(std::istream &is) { read(is, *this); }
+
+public: // operate on object
+    double balance();
+
+private: // private functions
+    void addinterest() { amount += amount * interestRate; }
+
+private: // nonstatic members
+    std::string owner;
+    double amount = 0.0;
+
+public: // operate on static
+    static double rate() { return interestRate; }
+
+    static void rate(double newRate) { interestRate = newRate; }
+
+    static bool allowinterest() { return interestDay; }
+
+    static void allowinterest(bool newDay) { interestDay = newDay; }
+
+private: // static members
+    static double interestRate;
+
+    static bool interestDay;
+};
+
+inline double Account::balance() {
+    if (interestDay)
+        addinterest();
+
+    return amount;
+}
+
+inline std::istream &read(std::istream &is, Account &obj) { return is >> obj.owner >> obj.amount; }
+
+inline std::ostream &print(std::ostream &os, const Account &obj) { return os << obj.owner << ' ' << obj.amount; }
+
+#endif
