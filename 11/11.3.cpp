af7a167cc3a880d3c3f43b89bcb405275bbdf621
Chandradeep Dey <chandradeepdey@posteo.net>
2020-02-08 02:13:40 +0530

    Add till 11.4

---
+++ 11/11.3.cpp
@@ -0,0 +1,19 @@
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <set>
+#include <iostream>
+
+int main() {
+    std::map<std::string, std::size_t> word_count;
+    std::set<std::string> exclude{"The", "But", "And", "Or", "An", "A", "the", "but", "and", "or", "an", "a"};
+    std::string word;
+    while (std::cin >> word) {
+        if (exclude.find(word) == exclude.end()) { ++word_count[word]; }
+    }
+    for (const auto &w : word_count) {
+        std::cout << w.first << " occurs " << w.second << (w.second == 1 ? " time\n" : " times\n");
+    }
+
+    return 0;
+}
---
+++ 11/11.4.cpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+int main() {
+    std::map<std::string, std::size_t> word_count;
+    std::set<std::string> exclude{"the", "but", "and", "or", "an", "a"};
+    std::string word;
+    while (std::cin >> word) {
+        for (auto &c : word) { c = std::tolower(c); }
+        word.erase(std::remove_if(word.begin(), word.end(), [](char ch) { return !std::isalnum(ch); }), word.end());
+        if (exclude.find(word) == exclude.end()) { ++word_count[word]; }
+    }
+    for (const auto &w : word_count) {
+        std::cout << w.first << " occurs " << w.second << (w.second == 1 ? " time\n" : " times\n");
+    }
+
+    return 0;
+}
