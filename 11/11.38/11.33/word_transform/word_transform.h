a6f1a76bd61a3aec4b432ca23df95577fe39b748
Chandradeep Dey <chandradeepdey@posteo.net>
2020-02-21 18:14:08 +0530

    Chapter 11 Done

---
+++ 11/11.38/11.33/word_transform/word_transform.h
@@ -0,0 +1,14 @@
+#ifndef C_PRIMER_5TH_WORD_TRANSFORM_H
+#define C_PRIMER_5TH_WORD_TRANSFORM_H
+
+#include <fstream>
+#include <unordered_map>
+#include <string>
+
+void word_transform(std::ifstream &map_file, std::ifstream &input);
+
+std::unordered_map<std::string, std::string> buildMap(std::ifstream &map_file);
+
+const std::string &transform(const std::string &s, const std::unordered_map<std::string, std::string> &m);
+
+#endif // C_PRIMER_5TH_WORD_TRANSFORM_H
