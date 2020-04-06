18c0d3846b58289c55fb3842ec8a0f4b808b0e8b
Chandradeep Dey <chandradeepdey@posteo.net>
2020-04-06 22:27:44 +0530

    Add till 14.7

---
+++ 14/14.2/Sales_data.h
@@ -0,0 +1,68 @@
+#ifndef C_PRIMER_5TH_SALES_DATA_H
+#define C_PRIMER_5TH_SALES_DATA_H
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+class Sales_data {
+    friend std::istream &operator>>(std::istream &is, Sales_data &item);
+
+    friend std::ostream &operator<<(std::ostream &os, const Sales_data &item);
+
+    friend Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs);
+
+public:
+    Sales_data() = default;
+
+    explicit Sales_data(std::string s, unsigned n = 0, double p = 0.0) : bookNo(std::move(s)), units_sold(n),
+                                                                         revenue(p * n) {}
+
+    explicit Sales_data(std::istream &is) { is >> *this; }
+
+public:
+    Sales_data &operator+=(const Sales_data &other);
+
+    double avg_price() const;
+
+    std::string isbn() const { return bookNo; }
+
+private:
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+inline double Sales_data::avg_price() const {
+    if (units_sold != 0) {
+        return revenue / units_sold;
+    } else {
+        return 0;
+    }
+}
+
+inline Sales_data &Sales_data::operator+=(const Sales_data &other) {
+    units_sold += other.units_sold;
+    revenue += other.revenue;
+    return *this;
+}
+
+inline std::istream &operator>>(std::istream &is, Sales_data &item) {
+    double price;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+inline std::ostream &operator<<(std::ostream &os, const Sales_data &item) {
+    return os << item.bookNo << ' ' << item.units_sold << ' ' << item.revenue << ' ' << item.avg_price();
+}
+
+inline Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs) {
+    Sales_data temp = lhs;
+    temp.units_sold += rhs.units_sold;
+    temp.revenue += rhs.revenue;
+    return temp;
+}
+
+#endif // C_PRIMER_5TH_SALES_DATA_H
---
+++ 14/14.7/String.cpp
@@ -0,0 +1,72 @@
+#include "String.h"
+
+#include <exception>
+#include <memory>
+
+// member functions
+String &String::operator=(const String &other) {
+    if (this != &other) {
+        if (std::allocator_traits<allocator_type>::propagate_on_container_copy_assignment::value) {
+            auto alloc_new = other.get_allocator();
+            auto ptr = _alloc_only(other.begin(), other.end(), alloc_new);
+            _free();
+            __alloc = alloc_new;
+            __first_element = ptr;
+            __first_free = __one_past_capacity = _copy_only(other.begin(), other.end(), __first_element);
+        } else {
+            auto data = _alloc_and_copy(other.begin(), other.end());
+            _free();
+            __first_element = data.first;
+            __first_free = __one_past_capacity = data.second;
+        }
+    }
+    return *this;
+}
+
+String &String::operator=(String &&other) {
+    if (this != &other) {
+        if (!std::allocator_traits<allocator_type>::propagate_on_container_move_assignment::value &&
+            __alloc != other.__alloc) {
+            return *this = other; // copy-assignment
+        } else {
+            _free();
+            if (std::allocator_traits<allocator_type>::propagate_on_container_move_assignment::value) {
+                __alloc = std::move(other.__alloc);
+            }
+            __first_element = other.__first_element;
+            __first_free = other.__first_free;
+            __one_past_capacity = other.__one_past_capacity;
+            other.__first_element = nullptr;
+        }
+    }
+    return *this;
+}
+
+// operations
+String &String::append(size_type count, CharT ch) {
+    if (count == 1) {
+        push_back(ch);
+    } else {
+        _check_and_reallocate(count);
+        for (size_type i = 0; i != count; ++i) { *__first_free++ = ch; }
+        *__first_free = '\0';
+    }
+    return *this;
+}
+
+// internal usage
+void String::_reallocate(size_type request) {
+    if (!request) { request = size() ? 2 * size() : 1; }
+    if (request > max_size()) {
+        throw std::length_error("String reallocation: requested size is greater than max_size()");
+    }
+    auto newbegin = std::allocator_traits<allocator_type>::allocate(__alloc, request + __account_for_null);
+    auto newend = newbegin;
+    auto old = __first_element;
+    for (size_type i = 0, upto = request < size() ? request : size(); i != upto; ++i) { *newend++ = *old++; }
+    *newend = '\0';
+    _free();
+    __first_element = newbegin;
+    __first_free = newend;
+    __one_past_capacity = __first_element + request;
+}
